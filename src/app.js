ac35e08322ff82e9a284e2a361400c5f409fbff9
Ashadul Islam <ashadulmridhaprog@gmail.com>
2024-09-29 13:56:20 +0600

    feat: dev endpoint add

--- src/app.js
+++ src/app.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
     message: "Welcome to the API!"
   });
 });
+app.get('/dev', (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the Dev!"
+  });
+});
 app.get('/hello', (req, res) => {
   res.status(200).json({
     message: "Hello World!"
